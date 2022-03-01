ceb5d375bb5ebe5d6323fb292412ef242021305f
escalope22 <victor.rambaud.85@gmail.com>
2022-03-01 17:00:29 +0100

    ajouter plage +1

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php include ("header.php");?>
-FOOT
+<section>
+    <div class="container">
+
+    <h1>Ajouter une plage</h1>
+    <form action="">
+
+        <div class="form-group">
+            <label for="nom">Le nom de la plage</label>
+            <input class="form-control" type="text" name="nom" placeholder="Plage">
+        </div>
+
+        <div class="form-group">
+            <label for="">Département</label>
+            <input class="form-control" type="text" name="" placeholder="Département">
+        </div>
+
+        <br>
+        <button class="btn btn-primary" type="submit">envoyer</button>
+
+    </form>
+    </div>
+
+</section>
 <?php include ("footer.php");?>
